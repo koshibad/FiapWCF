41be4fb09fe154f7aeeb73b4d3f70b1965bbe9bf
Diogo Koshiba <diogo.koshiba@gmail.com>
2017-04-24 22:07:09 -0300

    exercicio wcf

---
+++ LojaWCF/LojaService/IVendasService.cs
@@ -0,0 +1,18 @@
+﻿using LojaEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaService
+{
+    public interface IVendasService
+    {
+        [OperationContract]
+        List<Produto> GetProdutosEstoque();
+        [OperationContract]
+        void RegistrarVenda(Produto produto);
+    }
+}
---
+++ LojaWCF/LojaService/VendasService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using LojaEntities;
+
+namespace LojaService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class VendasService : IVendasService
+    {
+        readonly static List<Produto> _produtos = new List<Produto>()
+        {
+            new Produto { Id = 1, Nome = "Caneta" },
+            new Produto { Id = 2, Nome = "Papel" },
+            new Produto { Id = 3, Nome = "Lápis" },
+        };
+
+        public List<Produto> GetProdutosEstoque()
+        {
+            return _produtos;
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void RegistrarVenda(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
