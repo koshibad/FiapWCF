75669e250b171f66909ca09cf98841a39f4464e4
Diogo Koshiba <diogo.koshiba@gmail.com>
2017-04-24 22:19:46 -0300

    exercicio wcf

---
+++ LojaWCF/LojaSefHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using LojaService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaSefHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(VendasService)))
+            {
+                try
+                {
+                    host.Open();
+                    Console.WriteLine("Running.");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+                finally
+                {
+                    host.Close();
+                }
+            }
+        }
+    }
+}
--- LojaWCF/LojaService/IVendasService.cs
+++ LojaWCF/LojaService/IVendasService.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace LojaService
 {
+    [ServiceContract]
     public interface IVendasService
     {
         [OperationContract]
