0f3a95f68c172a199e90a3bb5715b8233b45fe2b
Diogo Koshiba <diogo.koshiba@gmail.com>
2017-05-03 21:37:49 -0300

    transaction + security

---
+++ AulasWCF_2/EscolaEntities/Nota.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscolaEntities
+{
+    [DataContract]
+    public class Nota
+    {
+        [DataMember(IsRequired = true)]
+        [Key, Column(Order = 0)]
+        public int AlunoId { get; set; }
+
+        [DataMember(IsRequired = true)]
+        [Key, Column(Order = 1)]
+        public int DisciplinaId { get; set; }
+
+        [DataMember(IsRequired = true)]
+        [Required]
+        public decimal Pontos { get; set; }
+
+        [IgnoreDataMember]
+        public bool Aprovado { get; set; }
+
+        [IgnoreDataMember]
+        public virtual Aluno Aluno { get; set; }
+        [IgnoreDataMember]
+        public virtual Disciplina Disciplina { get; set; }
+    }
+}
---
+++ AulasWCF_2/EscolaService/NotasServiceAdmin.cs
@@ -0,0 +1,42 @@
+﻿using EscolaEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EscolaService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class NotasServiceAdmin : INotasServiceAdmin, IDisposable
+    {
+        readonly EscolaContext _Context = new EscolaContext();
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void AtualizarNota(Nota nota)
+        {
+            var hostIdentity = WindowsIdentity.GetCurrent().Name;
+            var primaryIdentity = ServiceSecurityContext.Current.PrimaryIdentity.Name;
+            var windowsIdentity = ServiceSecurityContext.Current.WindowsIdentity.Name;
+            var threadIdentity = Thread.CurrentPrincipal.Identity.Name;
+
+            var notaDb = _Context.Notas.SingleOrDefault(n => n.AlunoId == nota.AlunoId &&
+                    n.DisciplinaId == nota.DisciplinaId);
+
+            if (notaDb != null)
+            {
+                notaDb.Pontos = nota.Pontos;
+
+                _Context.SaveChanges();
+            }
+        }
+
+        public void Dispose()
+        {
+            _Context.Dispose();
+        }
+    }
+}
